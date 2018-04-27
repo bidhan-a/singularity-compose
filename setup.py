21b240c890d9ff5ba811694d226ec0b947bdf4d2
bidhan-a <bidhan619@gmail.com>
2018-04-27 15:49:32 +0545

    Bump version: 0.1.4 → 1.0.0

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bidhan-a/compose-paas',
-    version='0.1.4',
+    version='1.0.0',
     zip_safe=False,
 )
